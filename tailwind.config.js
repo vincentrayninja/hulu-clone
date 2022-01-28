ae78c115f1719aac4e40be2d59f621d6efd686d7
vaibhavbuccha <vincent@ninjatechnolabs.com>
2022-01-28 12:32:23 +0530

    'HuluClone'

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
